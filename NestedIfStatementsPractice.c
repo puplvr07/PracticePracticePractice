17706898506754c014d7ebf9b79392017882635d
sean <seanroscady@gmail.com>
2025-08-26 23:07:27 +0800

    NestedIfStatementsPractice.c

--- NestedIfStatementsPractice.c
+++ NestedIfStatementsPractice.c
@@ -1,44 +1,3 @@
-/*#include <stdio.h>
-#include <math.h>
-#include <stdbool.h>
-
-int main() {
-
-    float price = 10.0;
-    bool Student = false ; //10% discount
-    bool Senior = false; //20% discount
-
-    printf("This is a program to calculate ticket prices with discounts using nested if statements! \n");
-
-    printf("Are you a student? (1 for yes, 0 for no): ");
-    scanf("%d", Student);
-    printf("Are you a senior? (1 for yes, 0 for no): ");
-    scanf("%d", Senior);
-
-    if(Student){
-        if(Senior){
-        printf("Student discount applied! (10% off)\n");
-        printf("Senior discount applied! (20% off)\n");
-        price = price * 0.7;
-    }
-        else{
-        printf("Student discount applied! (10% off)\n");
-        price = price * 0.9;
-        }
-    }
-    else {
-        if(Senior){
-        price = price * 0.8;
-        printf("Senior discount applied! (20% off)\n");
-    }
-
-    }
-
-    printf("Your total is: $%.2f\n", price);
-
-    return 0;
-}*/
-
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
@@ -81,4 +40,5 @@ int main() {
     printf("Your total is: $%.2f\n", price);
 
     return 0;
+
 }
