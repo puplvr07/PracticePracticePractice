bccb588c983c66601b72abe78f55fa20e099c219
sean <seanroscady@gmail.com>
2025-08-28 19:59:00 +0800

    C Practice

---
+++ ScrambleVariableScope.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <Windows.h>
+
+void scrambledWord(char word[]) {
+    char scrambled[50];            
+    strcpy(scrambled, word);       
+    int length = strlen(scrambled);
+
+    for (int i = 0; i < length; i++) {
+        int j = rand() % length;   // modulo sh
+        char temp = scrambled[i];  
+        scrambled[i] = scrambled[j];
+        scrambled[j] = temp;
+    }
+
+    printf("Your scrambled word is: %s\n", scrambled);
+}
+int main() {
+    srand(time(NULL)); // Random number generator 🤖
+
+    printf("This is a program that can scramble a word that you input! \n");
+
+    char user[100]; 
+    printf("Enter a word to scramble: ");
+    scanf("%s", user); 
+
+    printf("The original word is: %s\n", user);
+
+    printf("Working on it"); // decided to add just bc ;P
+    Sleep(800);
+    printf(".");
+    Sleep(800);
+    printf(".");
+    Sleep(1000);
+    printf(". \n");
+
+    scrambledWord(user);
+
+    return 0;
+}
+
+
